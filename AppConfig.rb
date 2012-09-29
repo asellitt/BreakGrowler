ca4095dfe00a8aeaf0346d1efb1d0e47fc98b43f
Anthony Sellitti <anthony.github@asellitt.com>
2012-09-30 01:33:05 +1000

    mplemented application configuration reader

---
+++ AppConfig.rb
@@ -0,0 +1,10 @@
+class AppConfig
+   require 'yaml'
+   
+   attr_reader :settings
+   
+   def initialize(config)
+      @settings = YAML.load_file(config)
+   end
+
+end
--- BreakGrowler.rb
+++ BreakGrowler.rb
@@ -1,8 +1,17 @@
 
 require 'ruby-growl'
 require 'ruby-growl/ruby_logo'
+require_relative 'AppConfig'
 
-g = Growl.new "localhost", "ruby-growl"
-g.add_notification("notification", "ruby-growl Notification",
-                   Growl::RUBY_LOGO_PNG)
-g.notify "notification", "It came from ruby-growl", "Greetings!"
+if __FILE__ == $PROGRAM_NAME
+   g = Growl.new("localhost", "ruby-growl")
+   g.add_notification("notification", "ruby-growl Notification", Growl::RUBY_LOGO_PNG)
+   g.notify("notification", "It came from ruby-growl", "Greetings!")
+   
+   config = AppConfig.new('config.yml')  
+   
+   puts config.settings['growl']['host']
+   puts config.settings['growl']['application']
+   config.settings['breaks'].each { |x| puts x['title'] }
+
+end
