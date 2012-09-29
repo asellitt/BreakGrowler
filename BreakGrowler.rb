55d35481692c7ccb18126cbfa00140a245da61fe
Anthony Sellitti <anthony.github@asellitt.com>
2012-09-30 02:08:09 +1000

    growls once per break in config

--- BreakGrowler.rb
+++ BreakGrowler.rb
@@ -4,14 +4,13 @@ require 'ruby-growl/ruby_logo'
 require_relative 'AppConfig'
 
 if __FILE__ == $PROGRAM_NAME
-   g = Growl.new("localhost", "ruby-growl")
-   g.add_notification("notification", "ruby-growl Notification", Growl::RUBY_LOGO_PNG)
-   g.notify("notification", "It came from ruby-growl", "Greetings!")
-   
    config = AppConfig.new('config.yml')  
    
    puts config.settings['growl']['host']
    puts config.settings['growl']['application']
    config.settings['breaks'].each { |x| puts x['title'] }
-
+   
+   g = Growl.new(config.settings['growl']['host'], config.settings['growl']['application'])
+   config.settings['breaks'].each { |x| g.add_notification(x['title'] + 'notification', config.settings['growl']['application'] + ' Notification', Growl::RUBY_LOGO_PNG) }
+   config.settings['breaks'].each { |x| g.notify(x['title'] + 'notification', x['title'], x['message'])}
 end
