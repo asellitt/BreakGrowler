92e61371079d0868412d74879333dd8219841729
Anthony Sellitti <anthony.github@asellitt.com>
2012-09-30 17:27:27 +1000

    removed sticky, added console prints

--- BreakGrowler.rb
+++ BreakGrowler.rb
@@ -4,21 +4,32 @@ require_relative 'AppConfig'
 require_relative 'Growler'
 
 if __FILE__ == $PROGRAM_NAME
+   puts 'BreakGrowler started'
+   
    config = AppConfig.new('config.yml')  
+   puts 'config loaded'
+   
    growler = Growler.new(
          config.settings['growl']['host'], 
          config.settings['growl']['application']
    );
-   scheduler = Rufus::Scheduler.start_new
+   puts 'growler initialised'
    
    # register the breaks
    config.settings['breaks'].each { |x| 
+      puts "registering #{x['title']}"
       growler.register(x['title'])
    }
    
+   scheduler = Rufus::Scheduler.start_new
+   puts 'scheduler started'
+   
    # schedule the notifications
    config.settings['breaks'].each { |x| 
+      puts "scheduling: #{x['title']} (every #{x['interval']})"
+      
       scheduler.every x['interval'] do
+         puts "sending notification: #{x['title']}"
          growler.notify(x['title'], x['message'])
       end
    }
--- Growler.rb
+++ Growler.rb
@@ -7,14 +7,13 @@ class Growler
    def initialize(host, application)
       # initialise Growl
       @growl = Growl.new(host, application);
-      @application = application;
    end
    
    def register(title)
-      @growl.add_notification("#{title} notification", "#{@application} Notification", Growl::RUBY_LOGO_PNG)
+      @growl.add_notification("#{title} notification", "#{title} Notification", Growl::RUBY_LOGO_PNG)
    end
    
    def notify(title, message)
-      @growl.notify("#{title} notification", title, message, 0, true)
+      @growl.notify("#{title} notification", title, message)
    end
 end
