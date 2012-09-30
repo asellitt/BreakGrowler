7c28b1fed75489b9babcc44302d20d73b75d0396
Anthony Sellitti <anthony.github@asellitt.com>
2012-09-30 16:29:54 +1000

    converts time entries in the config into time/unit hash entries

--- AppConfig.rb
+++ AppConfig.rb
@@ -5,6 +5,27 @@ class AppConfig
    
    def initialize(config)
       @settings = YAML.load_file(config)
+      
+      @settings['breaks'].each_index{ |index| 
+         # translate the time entries
+         translateTime!(index, 'duration')
+         translateTime!(index, 'interval')
+      }
+      
+      puts @settings.inspect
    end
 
+   private
+   def translateTime!(index, entry)
+      # retrieve the value
+      value = @settings['breaks'][index]
+      
+      # parse out the important parts
+      time, unit = value[entry][/[0-9]*/], value[entry][/[a-z]/]
+      
+      # store them in the settings
+      @settings['breaks'][index][entry] = {}
+      @settings['breaks'][index][entry]['time'] = time.to_i
+      @settings['breaks'][index][entry]['unit'] = unit
+   end
 end
--- BreakGrowler.rb
+++ BreakGrowler.rb
@@ -10,7 +10,18 @@ if __FILE__ == $PROGRAM_NAME
    puts config.settings['growl']['application']
    config.settings['breaks'].each { |x| puts x['title'] }
    
+   # growl for each break
    g = Growl.new(config.settings['growl']['host'], config.settings['growl']['application'])
-   config.settings['breaks'].each { |x| g.add_notification(x['title'] + 'notification', config.settings['growl']['application'] + ' Notification', Growl::RUBY_LOGO_PNG) }
-   config.settings['breaks'].each { |x| g.notify(x['title'] + 'notification', x['title'], x['message'])}
+   config.settings['breaks'].each { |x| 
+      # register notification
+      g.add_notification(x['title'] + 'notification', config.settings['growl']['application'] + ' Notification', Growl::RUBY_LOGO_PNG) 
+   }
+   
+   config.settings['breaks'].each { |x| 
+      # get time for each break
+      puts "interval: #{x['interval']['time']} #{x['interval']['unit']}"
+      puts "duration: #{x['duration']['time']} #{x['duration']['unit']}"
+      
+      g.notify(x['title'] + 'notification', x['title'], x['message'])
+   }
 end
